5993465d7a6698f94e296132eee7d1d44301f3af
zhaopengchao <582146778@qq.com>
2019-11-21 11:28:53 +0800

    新增 RocketMQ 消息中间件
    Signed-off-by: zhaopengchao <582146778@qq.com>

--- springcloud-util/src/main/java/com/dhcc/zpc/SpringCloudUtilApplication.java
+++ springcloud-util/src/main/java/com/dhcc/zpc/SpringCloudUtilApplication.java
@@ -1,7 +1,5 @@
 package com.dhcc.zpc;
 
-import com.dhcc.zpc.rocketmq.producer.MQProducerManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
